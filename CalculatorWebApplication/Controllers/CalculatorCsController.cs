78c48972a13e20bcd6699e8196c1aa3c79d6adb5
Hruthika <126941908+Hruthikaveluri@users.noreply.github.com>
2025-07-04 14:34:00 +0530

    Update CalculatorCsController.cs

--- CalculatorWebApplication/Controllers/CalculatorCsController.cs
+++ CalculatorWebApplication/Controllers/CalculatorCsController.cs
@@ -21,89 +21,89 @@ namespace CalculatorWebApplication.Controllers
             _context = context;
         }
 
-        // GET: api/CalculatorCs
-        //[HttpGet]
-        //public async Task<ActionResult<IEnumerable<CalculatorC>>> Getcalculator()
-        //{
-        //    return await _context.calculator.ToListAsync();
-        //}
+        GET: api/CalculatorCs
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CalculatorC>>> Getcalculator()
+        {
+           return await _context.calculator.ToListAsync();
+        }
 
-        //// GET: api/CalculatorCs/5
-        //[HttpGet("{id}")]
-        //public async Task<ActionResult<CalculatorC>> GetCalculatorC(int id)
-        //{
-        //    var calculatorC = await _context.calculator.FindAsync(id);
+        // GET: api/CalculatorCs/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<CalculatorC>> GetCalculatorC(int id)
+        {
+           var calculatorC = await _context.calculator.FindAsync(id);
 
-        //    if (calculatorC == null)
-        //    {
-        //        return NotFound();
-        //    }
+           if (calculatorC == null)
+           {
+               return NotFound();
+           }
 
-        //    return calculatorC;
-        //}
+           return calculatorC;
+        }
 
-        //// PUT: api/CalculatorCs/5
-        //// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        //[HttpPut("{id}")]
-        //public async Task<IActionResult> PutCalculatorC(int id, CalculatorC calculatorC)
-        //{
-        //    if (id != calculatorC.Id)
-        //    {
-        //        return BadRequest();
-        //    }
+        // PUT: api/CalculatorCs/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCalculatorC(int id, CalculatorC calculatorC)
+        {
+           if (id != calculatorC.Id)
+           {
+               return BadRequest();
+           }
 
-        //    _context.Entry(calculatorC).State = EntityState.Modified;
+           _context.Entry(calculatorC).State = EntityState.Modified;
 
-        //    try
-        //    {
-        //        await _context.SaveChangesAsync();
-        //    }
-        //    catch (DbUpdateConcurrencyException)
-        //    {
-        //        if (!CalculatorCExists(id))
-        //        {
-        //            return NotFound();
-        //        }
-        //        else
-        //        {
-        //            throw;
-        //        }
-        //    }
+           try
+           {
+               await _context.SaveChangesAsync();
+           }
+           catch (DbUpdateConcurrencyException)
+           {
+               if (!CalculatorCExists(id))
+               {
+                   return NotFound();
+               }
+               else
+               {
+                   throw;
+               }
+           }
 
-        //    return NoContent();
-        //}
+           return NoContent();
+        }
 
-        //// POST: api/CalculatorCs
-        //// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        //[HttpPost]
-        //public async Task<ActionResult<CalculatorC>> PostCalculatorC(CalculatorC calculatorC)
-        //{
-        //    _context.calculator.Add(calculatorC);
-        //    await _context.SaveChangesAsync();
+        // POST: api/CalculatorCs
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<CalculatorC>> PostCalculatorC(CalculatorC calculatorC)
+        {
+           _context.calculator.Add(calculatorC);
+           await _context.SaveChangesAsync();
 
-        //    return CreatedAtAction("GetCalculatorC", new { id = calculatorC.Id }, calculatorC);
-        //}
+           return CreatedAtAction("GetCalculatorC", new { id = calculatorC.Id }, calculatorC);
+        }
 
-        //// DELETE: api/CalculatorCs/5
-        //[HttpDelete("{id}")]
-        //public async Task<IActionResult> DeleteCalculatorC(int id)
-        //{
-        //    var calculatorC = await _context.calculator.FindAsync(id);
-        //    if (calculatorC == null)
-        //    {
-        //        return NotFound();
-        //    }
+        // DELETE: api/CalculatorCs/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCalculatorC(int id)
+        {
+           var calculatorC = await _context.calculator.FindAsync(id);
+           if (calculatorC == null)
+           {
+               return NotFound();
+           }
 
-        //    _context.calculator.Remove(calculatorC);
-        //    await _context.SaveChangesAsync();
+           _context.calculator.Remove(calculatorC);
+           await _context.SaveChangesAsync();
 
-        //    return NoContent();
-        //}
+           return NoContent();
+        }
 
-        //private bool CalculatorCExists(int id)
-        //{
-        //    return _context.calculator.Any(e => e.Id == id);
-        //}
+        private bool CalculatorCExists(int id)
+        {
+           return _context.calculator.Any(e => e.Id == id);
+        }
 
         
         
